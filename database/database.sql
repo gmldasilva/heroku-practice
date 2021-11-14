96eb68f293c7795a65d18062170f06765ab265d0
Gabriel Marcondes <gabriel.marcondes@zaig.com.br>
2021-11-14 09:52:31 -0300

    Removing unnecessary files

--- database/database.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-    id                          SERIAL PRIMARY KEY,
-    username                    VARCHAR(20) NOT NULL,
-    credential                  VARCHAR(50) NOT NULL,
-    created_at                  TIMESTAMP NOT NULL DEFAULT(now())
-);
